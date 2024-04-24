ce27a113bb88cb2fa7beb02b4ac6c63d4ed207e5
Thurue <arthurture8966@gmail.com>
2024-04-24 19:45:37 -0300

    criei o model e a tabela pelo migrate

--- lics/database/migrations/2024_04_24_213415_create_licitacoes_table.php
+++ lics/database/migrations/2024_04_24_213415_create_licitacoes_table.php
@@ -9,11 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('licitacoes', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_lic');
+            $table->integer('nu_fase')->default(-1);
+            $table->char('nu_edital', 80);
+            $table->char('modalidade', 255);
+            $table->timestamp('data_abertura');
+            $table->char('nome_licitador', 255)->nullable();
+            $table->char('cnpj_licitador', 25)->nullable();
+            $table->smallInteger('prioridade');
+            $table->text('objeto');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
